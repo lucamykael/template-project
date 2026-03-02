04b06c548bb3b4efb800fbcf231b0447437b2423
Lucas Mykael <myklpm@gmail.com>
2026-03-01 22:26:01 -0400

    Update main file

---
+++ src/main/java/com/dev/template/model/Application.java
@@ -0,0 +1,11 @@
+package ${BASE_PACKAGE};
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(${PROJECT_NAME} $.Application.class, args);
+  }
+
+}
