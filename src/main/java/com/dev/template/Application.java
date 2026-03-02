a1dfe04ef27538164f10158e20e565287fdc2b1e
Lucas Mykael <myklpm@gmail.com>
2026-03-01 22:58:14 -0400

    Change folders

--- src/main/java/com/dev/template/Application.java
+++ src/main/java/com/dev/template/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(${PROJECT_NAME} Application.class, args);
+    SpringApplication.run(PROJECT_NAMEApplication.class, args);
   }
 
 }
